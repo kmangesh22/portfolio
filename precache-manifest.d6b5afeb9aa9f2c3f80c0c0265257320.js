584c5b1567933816dbafce96fd9c15c9955a5cba
Mangesh Kumar <kumarmangesh55@gmail.com>
2019-04-06 09:46:48 +0530

    Updates

---
+++ precache-manifest.d6b5afeb9aa9f2c3f80c0c0265257320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8a1cb5f81201ffac4ac965efc461337",
+    "url": "/kmangesh49/portfolio/static/media/programmer.f8a1cb5f.png"
+  },
+  {
+    "revision": "d6b488f26b7b86811fda",
+    "url": "/kmangesh49/portfolio/static/js/runtime~main.d6b488f2.js"
+  },
+  {
+    "revision": "a77b8132a2e407db257d",
+    "url": "/kmangesh49/portfolio/static/js/main.a77b8132.chunk.js"
+  },
+  {
+    "revision": "804d4ef2c4b557815b86",
+    "url": "/kmangesh49/portfolio/static/js/1.804d4ef2.chunk.js"
+  },
+  {
+    "revision": "a77b8132a2e407db257d",
+    "url": "/kmangesh49/portfolio/static/css/main.3200809e.chunk.css"
+  },
+  {
+    "revision": "804d4ef2c4b557815b86",
+    "url": "/kmangesh49/portfolio/static/css/1.cca9271c.chunk.css"
+  },
+  {
+    "revision": "d37d8d1c8b40fe0bb5950f6dbcea28f5",
+    "url": "/kmangesh49/portfolio/index.html"
+  }
+];
